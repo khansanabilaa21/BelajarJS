4d77973a4708e33d7240dbd64e35b0528d2f75b7
khansanabilaa21 <khansanabilaa21@upi.edu>
2023-10-12 16:52:37 +0300

    add index.html

---
+++ main.js
@@ -0,0 +1,36 @@
+//console.log("Hello World")
+//alert("Ada Notifikasi")
+//prompt("Pinjam Seratus")
+
+//var Promnet = "Coding is Easy"
+//console.log(Promnet)
+//var Promnet = "Coding not Easy"
+//console.log(Promnet)
+
+//let Promnet = "Coding is Fun"
+//console.log(Promnet)
+
+//const Promnet = "Coding is My Life"
+//console.log(Promnet);
+//const.Promnet= "Coding not My Life"
+//console.log(Promnet);
+
+//let totalPoin = prompt("Masukkan Poin Anda")
+    //if(totalPoin >= 100){
+        //document.write("Congratulation");
+        //}else{
+            //document.write("Keep Trying")
+    //}
+
+//let x = 6;
+//let y = 3;
+
+//console.log(x<10 && y > 1);
+//console.log(x<10 && y < 1);
+//console.log(x==5||y==5);
+//console.log(x==6||y==5);
+//console.log(!(x==y));
+
+//document.write(x<10 && y > 1);
+//document.write("<br>");
+//document.write(x<10 && y < 1);
